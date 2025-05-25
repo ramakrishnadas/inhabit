9452c9b9a8d613ca6334560e97835aa676ba51bb
reginmund9 <moronitaca@gmail.com>
2025-05-24 19:55:15 -0600

    ramoncin-w3

---
+++ src/app/lib/placeholderdata..js
@@ -0,0 +1,19 @@
+const sampleUsers = [
+  {
+    name: "Rama Bhagi",
+    email: "rama@gmail.com",
+    password: "12345678",
+    image: "thumbnail.com"
+  }
+]
+
+const samplehabits = [
+  {
+    user_id: "2603b53c-292f-4193-814d-4d0c5c265f7e",
+    name: "learn Chinese",
+    target_amount: 15,
+    unit: "minutes",
+    frequency: "dayly"
+  }
+]
+
