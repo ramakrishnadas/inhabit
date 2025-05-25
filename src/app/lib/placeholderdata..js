822fcf50697d0796380e1c78a0a262046a23350a
reginmund9 <moronitaca@gmail.com>
2025-05-24 23:47:04 -0600

    data-week3

--- src/app/lib/placeholderdata..js
+++ src/app/lib/placeholderdata..js
@@ -13,7 +13,17 @@ const samplehabits = [
     name: "learn Chinese",
     target_amount: 15,
     unit: "minutes",
-    frequency: "dayly"
+    frequency: "daily"
+  },
+  
+  {
+    user_id: "2603b53c-292f-4193-814d-4d0c5c265f7e",
+    name: "drink water",
+    target_amount: 8,
+    unit: "glasses",
+    frequency: "daily"
   }
+
 ]
+
 
